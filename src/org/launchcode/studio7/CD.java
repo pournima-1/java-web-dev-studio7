652dd2490630bf4b6ca7e73ac01b25732636a51f
Pournima Rajagopal <pourni.r@gmail.com>
2021-05-30 10:34:36 -0500

    Init

--- src/org/launchcode/studio7/CD.java
+++ src/org/launchcode/studio7/CD.java
@@ -4,7 +4,6 @@ public class CD {
 
     // TODO: Implement your custom interface.
 
-
     // TODO: Determine which fields, methods, and constructors can be extended from the base class and which ones
     //  need to be declared separately.
 
